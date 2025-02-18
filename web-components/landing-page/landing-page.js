d1008e4f81864edd2a4eb7e3e994e3fc04f9db80
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2025-02-18 11:57:58 +0200

    Added default project files for research-tool

---
+++ web-components/landing-page/landing-page.js
@@ -0,0 +1,17 @@
+export class LandingPage {
+    constructor(element, invalidate) {
+        this.element = element;
+        this.invalidate = invalidate;
+
+        this.invalidate(async () => {
+        });
+    }
+
+    async beforeRender() {
+
+    }
+
+    async afterRender() {
+
+    }
+}
